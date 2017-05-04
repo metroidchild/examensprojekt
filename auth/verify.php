15bc6a6b922ff69d2a3966e846f1f879b3e6a674
Emil Lindgren <emil__1@hotmail.com>
2017-05-04 11:24:41 +0200

    Lade till mer info för att debugga varför login inte fungerar

--- auth/verify.php
+++ auth/verify.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!isset($_GET['user']) || !isset($_GET['password'])) {
-    echo "ERROR V1: Alla fält inte satta!";
+    echo "ERROR V1: Alla fält inte satta!\r\n";
     exit;
 }
 
@@ -9,18 +9,21 @@ $user;
 
 // Notera att prestanda minskar med antalet användare i json filen,
 // men det fungerar som en exempel-databas
-$json = file_get_contents("users.json");
+$json = file_get_contents('users.json');
 if ($json === false) {
-    echo "ERROR V2: Kan inte komma åt databas!";
+    echo "ERROR V2: Kan inte komma åt databas!\r\n";
     exit;
 } else {
+    echo "Hittade databas!\r\n";
     // Sätt in innehållet av users arrayen i users
     // Om användare finns, sätt $user till den arrayen
-    $user = isset(json_decode($json)[$_GET['user']]) ? json_decode($json)[$_GET['user']] : false;
+    $user = (isset(json_decode($json)[$_GET['user']]) ? json_decode($json)[$_GET['user']] : false);
     if ($user == false) {
         // Ge samma fel vid fel användarnamn eller lösenord
-        echo "ERROR V3: Fel namn eller lösenord!";
+        echo "ERROR V3: Fel namn eller lösenord!\r\n";
         exit;
+    } else {
+        echo "Hittade användare!\r\n";
     }
 }
 
@@ -28,9 +31,10 @@ $salt = "!@^-.4"; // Varning, detta är inte en säker lösning, följ kryptogra
                   // hjälpsidor för att säkrare kryptera lösenord
 if (strtolower(hash("sha256", $salt+$_GET['password'])) != strtolower($user['password'])) {
     // Ge samma fel vid fel användarnamn eller lösenord
-    echo "ERROR V3: Fel namn eller lösenord!";
+    echo "ERROR V3: Fel namn eller lösenord!\r\n";
     exit;
 } else {
+    echo "Rätt lösenord!\r\n";
     // Användare är inloggad
     // I ett verkligt scenario, ge tillbaka en token och spara samma token i en relationsdatabas till users,
     // för att öka säkerheten och skapa en potentiell automatisk inloggning
