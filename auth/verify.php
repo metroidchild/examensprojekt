8a18898ba46b26aaa237fd8295512af17081532f
Emil Lindgren <emil__1@hotmail.com>
2017-05-04 12:48:34 +0200

    Ändrade tillbaka saker efter att ha fått dom att fungera

--- auth/verify.php
+++ auth/verify.php
@@ -1,7 +1,6 @@
 <?php
 
-echo "<pre>\r\n";
-
+// Försäkra oss att både användarnamn och lösenord är tillgängliga
 if (!isset($_GET['user']) || !isset($_GET['password'])) {
     echo "ERROR V1: Alla fält inte satta!\r\n";
     exit;
@@ -16,31 +15,24 @@ if ($json === false) {
     echo "ERROR V2: Kan inte komma åt databas!\r\n";
     exit;
 } else {
-    echo "Hittade databas!\r\n";
     // Sätt in innehållet av users arrayen i users
     // Om användare finns, sätt $user till den arrayen
     $decode = json_decode($json);
-    var_dump($decode);
-    var_dump($decode->{'users'});
-    var_dump($decode->{'users'}->{$_GET['user']});
-    $user = (isset($decode['users'][$_GET['user']]) ? $decode['users'][$_GET['user']] : false);
+    $user = (isset($decode->{'users'}->{$_POST['user']}) ? $decode->{'users'}->{$_POST['user']} : false);
     if ($user == false) {
         // Ge samma fel vid fel användarnamn eller lösenord
         echo "ERROR V3: Fel namn eller lösenord!\r\n";
         exit;
-    } else {
-        echo "Hittade användare!\r\n";
     }
 }
 
 $salt = "!@^-.4"; // Varning, detta är inte en säker lösning, följ kryptografiska
                   // hjälpsidor för att säkrare kryptera lösenord
-if (strtolower(hash("sha256", $salt+$_GET['password'])) != strtolower($user['password'])) {
+if (strtolower(hash("sha256", $salt+$_POST['password'])) != strtolower($user->{'password'})) {
     // Ge samma fel vid fel användarnamn eller lösenord
     echo "ERROR V3: Fel namn eller lösenord!\r\n";
     exit;
 } else {
-    echo "Rätt lösenord!\r\n";
     // Användare är inloggad
     // I ett verkligt scenario, ge tillbaka en token och spara samma token i en relationsdatabas till users,
     // för att öka säkerheten och skapa en potentiell automatisk inloggning
