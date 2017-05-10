03b71f8027b1be2f0da11fdf481cad2ec7d4296f
Emil Lindgren <emil__1@hotmail.com>
2017-05-10 10:53:55 +0200

    Ändrade index sidan och lade till nedladdningar

--- index.php
+++ index.php
@@ -4,7 +4,33 @@
 </head>
 <body>
 
-Hello Webbrowser!
+<form method="get" action="DRMworld.exe">
+<button type="submit">Ladda ner DRMworld här</button>
+</form>
+<form method="get" action="DRMworld.exe">
+<button type="submit">Källkod som GameMaker: Studio projekt</button>
+</form>
+
+<h3>Hur man spelar</h3>
+<p>
+    Klicka <pre>Shift + Enter</pre> för att simulera ett spel utan inloggning.<br>
+    <br>
+    Spela genom första banan (försök att ta den rosa stjärnan) och bli tillbakaskickad till login skärmen
+</p>
+<p>
+    Klicka <pre>F12 + Enter</pre> för att simulera en inloggning.<br>
+    Alternativt logga in med användarnamn "testuser" och lösenord "password",<br>
+    eller användarnamn "testuser2" och lösenord "albatross"<br>
+    <br>
+    Försök igen att ta den rosa stjärnan och se att du nu tog dig till en ny bana,<br>
+    det finns inget fint sätt att stänga av spelet, så kryssa bara ner det.
+</p>
+
+<h3>Hur man kollar på koden</h3>
+<p>
+    GameMaker: Studio 1.4 går inte längre att få tag i, men jag vill inte konvertera till GM: S 2.0 före arbetet är slutfört.<br>
+    Det går att döpa om .gmz till .zip och öppna upp projektet, men koden är inte lätt att följa om man inte har en editor.
+</p>
 
 </body>
 </html>
